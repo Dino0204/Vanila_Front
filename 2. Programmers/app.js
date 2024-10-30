ea38e2da539426ad66cfa9fc1efed6e4e99587ce
Dino0204 <s24020@gsm.hs.kr>
2024-10-30 23:49:53 +0900

    feat & fix: Js 파일 업로드 및 쓸모없는 파일 삭제

---
+++ 2. Programmers/app.js
@@ -0,0 +1,36 @@
+// const code = ['a','b','c','1','a','b','c','1','a','b','c'];
+
+// function solution(code) {
+    
+//     const Len = code.length;
+//     let mode = 0;
+//     let ret = [];
+//     for(let idx = 0;idx < Len;idx++){
+//         if(mode === 0){
+//             if(code[idx] !== '1' && idx%2 === 0){
+//                 ret[idx] + code[idx];
+//             }else{
+                
+//             }
+//         }else if(mode === 1){
+
+//         }
+//     }
+//     if(ret === ''){
+//         return "EMPTY";
+//     }else{
+//         return  'ret' + ret;
+//     }
+// }
+
+if(confirm()===true){
+    console.log('true를 반환했습니다.');
+}else{
+    console.log('false를 반환했습니다.');
+}
+
+
+
+
+
+
---
+++ 3. Js Practice/jsx.js
@@ -0,0 +1,15 @@
+//var b;
+
+// function sayHello() {
+// 	console.log('Hello')
+// }
+
+// console.log('Hello!')
+
+// let a = 1;
+// b = 2;
+
+// const myName = function() {
+// 	console.log('My name is Sophia.')
+// }
+
